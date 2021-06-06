6e53c6a9b123beebdb75f61ebdc5a293e284a416
Bhanu-183 <bhanukrishna2019@iiitkottayam.ac.in>
2021-06-06 22:45:47 +0530

    Added Form

--- firstproject/firstapp/urls.py
+++ firstproject/firstapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('users',views.users,name='users')
+    path('users', views.users, name='users'),
+    path('form',views.form_name_view,name='form'),
 ]
--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from . import forms
 from firstapp.models import Topic,Webpage,AccessRecord,User
 
 def index(request):
@@ -8,8 +8,19 @@ def index(request):
     date_dict={'records':webpage_list}
     return render(request,'index.html',context=date_dict)
 
-
 def users(request):
     user_list = User.objects.order_by('fname')
     user_dict = {'users': user_list}
-    return render(request,'users.html',context=user_dict)
+    return render(request, 'users.html', context=user_dict)
+    
+def form_name_view(request):
+    form = forms.FormName()
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            print("Name:", name)
+            print("Email", form.cleaned_data['email'])
+            print("Text:",form.cleaned_data['text'])
+    return render(request, 'form_page.html', context={'form': form})
+    
